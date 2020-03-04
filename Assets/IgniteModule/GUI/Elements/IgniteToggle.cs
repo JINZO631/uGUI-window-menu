54bdb67e5dc655508909193152c88c805be3a483
JINZO <syntheticseraphim631@gmail.com>
2020-03-04 18:09:38 +0900

    ToggleにreadOnlyを追加する

--- Assets/IgniteModule/GUI/Elements/IgniteToggle.cs
+++ Assets/IgniteModule/GUI/Elements/IgniteToggle.cs
@@ -23,7 +23,7 @@ namespace IgniteModule
             backgroundImage.rectTransform.SetSizeDelta(height, height);
         }
 
-        public static IgniteToggle Create(Action<bool> onValueChanged, bool defaultValue)
+        public static IgniteToggle Create(Action<bool> onValueChanged, bool defaultValue, bool readOnly)
         {
             var instance = Instantiate(Resources.Load<GameObject>("IgniteGUI/Toggle")).GetComponent<IgniteToggle>();
 
@@ -34,6 +34,7 @@ namespace IgniteModule
             instance.toggle.onValueChanged.AddListener(new UnityAction<bool>(onValueChanged));
             instance.toggle.onValueChanged.AddListener(v => instance.onSelected.Invoke());
             instance.toggle.onValueChanged.AddListener(v => instance.checkmarkImage.enabled = v);
+            instance.toggle.interactable = !readOnly;
 
             return instance;
         }
@@ -41,19 +42,19 @@ namespace IgniteModule
 
     public static partial class IIgniteGUIGroupExtensions
     {
-        public static IIgniteGUIGroup AddToggle(this IIgniteGUIGroup group, Action<bool> onValueChanged, bool defaultValue = true)
+        public static IIgniteGUIGroup AddToggle(this IIgniteGUIGroup group, Action<bool> onValueChanged, bool defaultValue = true, bool readOnly = false)
         {
-            return group.Add(IgniteToggle.Create(onValueChanged, defaultValue));
+            return group.Add(IgniteToggle.Create(onValueChanged, defaultValue, readOnly));
         }
 
-        public static IIgniteGUIGroup AddToggle(this IIgniteGUIGroup group, string label, Action<bool> onValueChanged, bool defaultValue = true)
+        public static IIgniteGUIGroup AddToggle(this IIgniteGUIGroup group, string label, Action<bool> onValueChanged, bool defaultValue = true, bool readOnly = false)
         {
-            return group.Add(IgniteHorizontalGroup.Create().AddToggle(onValueChanged, defaultValue).AddLabel(label) as IgniteHorizontalGroup);
+            return group.Add(IgniteHorizontalGroup.Create().AddToggle(onValueChanged, defaultValue, readOnly).AddLabel(label) as IgniteHorizontalGroup);
         }
 
-        public static IIgniteGUIGroup AddToggleWithButton(this IIgniteGUIGroup group, string buttonName, Action<bool> onClick, Action<bool> onValueChanged = null, bool defaultValue = true)
+        public static IIgniteGUIGroup AddToggleWithButton(this IIgniteGUIGroup group, string buttonName, Action<bool> onClick, Action<bool> onValueChanged = null, bool defaultValue = true, bool readOnly = false)
         {
-            var toggle = IgniteToggle.Create(onValueChanged ?? delegate { }, defaultValue);
+            var toggle = IgniteToggle.Create(onValueChanged ?? delegate { }, defaultValue, readOnly);
 
             var horizontalGroup = IgniteHorizontalGroup.Create().Add(toggle).AddButton(buttonName, () => onClick(toggle.IsOn)) as IgniteHorizontalGroup;
 
